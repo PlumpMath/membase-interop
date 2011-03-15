665300873d587f1f09f76dfb264c7fb680e75cdd
Attila Kiskó <a@enyim.com>
2011-03-15 14:27:06 +0100

    Version bump.

--- Membase.Interop/Properties/AssemblyInfo.cs
+++ Membase.Interop/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Membase.Interop")]
-[assembly: AssemblyCopyright("Copyright © 2010")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,11 +20,11 @@ using System.Runtime.InteropServices;
 // Major & Minor reflects the version number of the Membase library
 // Revision is incremented for each release of the Interop library
 //
-// 4th release of the Interop library, linked to the 2.10 of Mmebase:
+// 4th release of the Interop library, linked to the 2.10 of Membase:
 //
 //	2.10.0.4
 //
-[assembly: AssemblyVersion("2.10.0.5")]
-[assembly: AssemblyFileVersion("2.10.0.5")]
+[assembly: AssemblyVersion("2.11.0.1")]
+[assembly: AssemblyFileVersion("2.11.0.1")]
 
 [assembly: AssemblyKeyName("enyim")]
